e57bbfe66b826a4ccefeceb83da2a6d3cbb265eb
Philippe Fillion <pfi@sitecore.net>
2018-10-30 13:12:37 -0400

    Initial Commit

---
+++ src/Sitecore.Support.289668/ContentTesting/Pipelines/GetTestToRun/EnsurePageTest.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Sitecore.Support.ContentTesting.Pipelines.GetTestToRun
+{
+  using Sitecore.ContentTesting.Pipelines.GetTestToRun;
+  using Sitecore.ContentTesting.Model;
+  using System.Collections.Specialized;
+  using System.Web;
+  public class EnsurePageTest : GetTestToRunProcessor
+  {
+    public override void Process(GetTestToRunArgs args)
+    {
+      //Fix for bug 289668 : added check for TestType.
+      if (args.TestDefinition != null && args.TestDefinition.GetTestType() == TestType.Page)
+      {
+        string uri = args.HostItem.Uri.ToDataUri().ToString();
+        uri = RemoveVersionFromUri(uri);
+        string text = args.TestDefinition.ParseContentItem()?.ToString();
+        if (text != null)
+        {
+          text = RemoveVersionFromUri(text);
+          if (!text.Equals(uri))
+          {
+            args.TestDefinition = null;
+          }
+        }
+      }
+    }
+
+    private string RemoveVersionFromUri(string uri)
+    {
+      if (string.IsNullOrEmpty(uri))
+      {
+        return uri;
+      }
+      NameValueCollection nameValueCollection = HttpUtility.ParseQueryString(uri);
+      nameValueCollection.Remove("ver");
+      return nameValueCollection.ToString();
+    }
+  }
+}
---
+++ src/Sitecore.Support.289668/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.289668")]
+[assembly: AssemblyProduct("Sitecore.Support.289668")]
+[assembly: ComVisible(false)]
